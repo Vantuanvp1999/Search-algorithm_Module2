4439aac8c03e558beb03042b21efc87b0600bb34
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-23 09:16:44 +0700

    addth1

---
+++ Demo/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int arr[] ={2,4,12,43,98,3,6,8,9,34,54,76};
+        System.out.println("Chỉ số của 9:");
+        System.out.println(search(arr,9));
+        System.out.println("Chỉ số của 54:");
+        System.out.println(search(arr,54));
+        System.out.println("Chỉ số của 12:");
+        System.out.println(search(arr,12));
+
+    }
+    public static int search(int[] arr, int target) {
+        int index=-1;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]==target) {
+                index=i;
+            }
+        }return index;
+    }
+}
---
+++ ThucHanhBai1/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+
+    }
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+    static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            }else if (arr[mid] < key) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+
+        }return -1;
+    }
+}
